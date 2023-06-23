7b46fb60d1cc812f2eb70e97a6f732344f40d5b7
jaggerabney <jagger.abney@gmail.com>
2023-06-23 15:17:47 -0700

    items on Meetup page are now fetched from firebase

--- misc/reactjs-refresher-2/src/pages/Meetups/Meetups.js
+++ misc/reactjs-refresher-2/src/pages/Meetups/Meetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../../components/meetups/MeetupList/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -22,10 +23,37 @@ const DUMMY_MEETUPS = [
 ];
 
 function MeetupsPage() {
+  const [meetups, setMeetups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/meetups.json`
+      );
+      const meetupsData = await response.json();
+      let meetupsArray = [];
+
+      for (const key in meetupsData) {
+        meetupsArray.push({
+          id: key,
+          ...meetupsData[key],
+        });
+      }
+
+      setIsLoading(false);
+      setMeetups(meetupsArray);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      {isLoading ? <p>Loading...</p> : <MeetupList meetups={meetups} />}
     </section>
   );
 }
