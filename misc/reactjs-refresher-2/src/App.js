3cd2740cf58fa025a7be4dee74aa0feac39e077b
jaggerabney <jagger.abney@gmail.com>
2023-06-23 13:45:00 -0700

    added navbar content

--- misc/reactjs-refresher-2/src/App.js
+++ misc/reactjs-refresher-2/src/App.js
@@ -3,20 +3,24 @@ import { Route, Switch } from "react-router-dom";
 import MeetupsPage from "./pages/Meetups/Meetups";
 import NewMeetupPage from "./pages/NewMeetup/NewMeetup";
 import FavoritesPage from "./pages/Favorites/Favorites";
+import MainNavigation from "./components/layout/MainNavigation/MainNavigation";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <MeetupsPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
-    </Switch>
+    <>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <MeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
