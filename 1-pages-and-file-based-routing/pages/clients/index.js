3581cac6b7bedf6840d220fe5e308f6e67d491bd
jaggerabney <jagger.abney@gmail.com>
2023-06-27 16:01:59 -0700

    added navbars to home/clients page

--- 1-pages-and-file-based-routing/pages/clients/index.js
+++ 1-pages-and-file-based-routing/pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Max" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>Clients page!</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
