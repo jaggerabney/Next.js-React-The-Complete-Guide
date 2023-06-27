77f29674cad57aff586a34c46b19cb33eea34aae
jaggerabney <jagger.abney@gmail.com>
2023-06-27 16:13:32 -0700

    added programmatic navigation

--- 1-pages-and-file-based-routing/pages/clients/[clientId]/index.js
+++ 1-pages-and-file-based-routing/pages/clients/[clientId]/index.js
@@ -3,10 +3,17 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
 
+  function loadProjectHandler() {
+    // load project
+
+    router.push(`/clients/${router.query.clientId}/project1`);
+  }
+
   return (
     <div>
       <h1>Client projects page!</h1>
       <p>Client ID: {router.query.clientId}</p>
+      <button onClick={loadProjectHandler}>Load first project</button>
     </div>
   );
 }
