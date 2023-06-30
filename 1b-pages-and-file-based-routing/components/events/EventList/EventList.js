7ab031197e94e395ba69349bfcc931b1ab2986f2
jaggerabney <jagger.abney@gmail.com>
2023-06-29 17:27:11 -0700

    added styling for EventItem/EventList components

--- 1b-pages-and-file-based-routing/components/events/EventList/EventList.js
+++ 1b-pages-and-file-based-routing/components/events/EventList/EventList.js
@@ -1,8 +1,10 @@
 import EventItem from "../EventItem/EventItem";
 
+import classes from "./EventList.module.css";
+
 function EventList(props) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {props.events.map((event) => (
         <EventItem key={event.id} event={event} />
       ))}
