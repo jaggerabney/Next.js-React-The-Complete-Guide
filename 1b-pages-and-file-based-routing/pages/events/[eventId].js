a8daa5f67344ce1a67ebb23b504e9cbd092660e8
jaggerabney <jagger.abney@gmail.com>
2023-06-29 17:49:43 -0700

    added components and content to event detail page

--- 1b-pages-and-file-based-routing/pages/events/[eventId].js
+++ 1b-pages-and-file-based-routing/pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import { useRouter } from "next/router";
+
+import EventSummary from "../../components/events/EventSummary/EventSummary";
+import EventLogistics from "../../components/events/EventLogistics/EventLogistics";
+import EventContent from "../../components/events/EventContent/EventContent";
+import { getEventById } from "../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event detail page!</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
 
