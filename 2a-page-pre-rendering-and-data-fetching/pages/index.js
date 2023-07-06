301ac97b5c20de2d66d0c14a3d9b67fdc34251c4
jaggerabney <jagger.abney@gmail.com>
2023-07-05 17:40:18 -0700

    explored static generation

--- 2a-page-pre-rendering-and-data-fetching/pages/index.js
+++ 2a-page-pre-rendering-and-data-fetching/pages/index.js
@@ -1,4 +1,4 @@
-function HomePage(props) {
+function HomePage({ products }) {
   return (
     <ul>
       <li>Product 1</li>
@@ -6,6 +6,14 @@ function HomePage(props) {
       <li>Product 3</li>
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
 }
 
 export default HomePage;
