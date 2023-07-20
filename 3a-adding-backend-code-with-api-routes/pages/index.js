a77b65d0be285214b534764d4354ab5688120e31
jaggerabney <jagger.abney@gmail.com>
2023-07-20 11:27:32 -0700

    explored creating api routes; added form to page

--- 3a-adding-backend-code-with-api-routes/pages/index.js
+++ 3a-adding-backend-code-with-api-routes/pages/index.js
@@ -1,7 +1,33 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredFeedback = feedbackRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <br />
+          <input type="email" id="email" ref={emailRef} />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <br />
+          <textarea id="feedback" rows="5" ref={feedbackRef} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
