85d68c13656aadd58fc5dd6889cd218a2095453c
jaggerabney <jagger.abney@gmail.com>
2023-07-20 11:39:36 -0700

    added feedback.json/POST route for "/api/feedback"

--- 3a-adding-backend-code-with-api-routes/pages/api/feedback.js
+++ 3a-adding-backend-code-with-api-routes/pages/api/feedback.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({ message: "Hello from the backend!" });
+  if (req.method === "POST") {
+    const { email, feedback } = req.body;
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback,
+    };
+
+    const data = fs.readFileSync(filePath);
+    const allFeedback = JSON.parse(data);
+
+    allFeedback.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(allFeedback));
+
+    res.status(201).json({ message: "Feedback added!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "Hello from the backend!" });
+  }
 }
 
 export default handler;
