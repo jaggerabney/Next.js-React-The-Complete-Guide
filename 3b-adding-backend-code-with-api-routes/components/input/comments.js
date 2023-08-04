922404aac035c1c97eb49a144d0a0db0e1dfd92d
jaggerabney <jagger.abney@gmail.com>
2023-08-04 16:29:43 -0700

    comments now having loading text/use notifs

--- 3b-adding-backend-code-with-api-routes/components/input/comments.js
+++ 3b-adding-backend-code-with-api-routes/components/input/comments.js
@@ -1,26 +1,39 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
+import NotificationContext from "../../store/notification-context";
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
+
 import classes from "./comments.module.css";
 
 function Comments({ eventId }) {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const notifContext = useContext(NotificationContext);
 
   useEffect(() => {
-    if (showComments) {
+    if (showComments && comments.length === 0) {
       fetch(`/api/comments/${eventId}`)
         .then((response) => response.json())
-        .then((data) => setComments(data.comments));
+        .then((data) => {
+          setComments(data.comments);
+          setIsLoading(false);
+        });
     }
-  }, [showComments]);
+  }, [showComments, comments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
+    notifContext.showNotification({
+      title: "Adding comment...",
+      message: "Adding comment to post...",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -28,9 +41,32 @@ function Comments({ eventId }) {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notifContext.showNotification({
+          title: "Success!",
+          message: data.message,
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notifContext.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      });
   }
+
+  console.log(showComments, isLoading);
 
   return (
     <section className={classes.comments}>
@@ -38,7 +74,8 @@ function Comments({ eventId }) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList comments={comments} />}
+      {showComments && isLoading && <p>Loading...</p>}
+      {showComments && !isLoading && <CommentList comments={comments} />}
     </section>
   );
 }
