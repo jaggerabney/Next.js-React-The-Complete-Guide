9b2dc68ea2d50e62865c65743779c96c3ea01d9f
jaggerabney <jagger.abney@gmail.com>
2023-08-13 22:38:19 -0700

    blog posts can now include code snippets

--- course-project/components/posts/PostDetail/PostDetail.js
+++ course-project/components/posts/PostDetail/PostDetail.js
@@ -1,22 +1,17 @@
 import Image from "next/image";
 
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./PostDetail.module.css";
 
 function PostContent({ post }) {
   const reactMarkdownRenderers = {
-    img(image) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-        />
-      );
+    img({ src, alt }) {
+      return <Image src={`/images/posts/${post.slug}/${src}`} alt={alt} />;
     },
-    p(paragraph) {
-      const { node } = paragraph;
-
+    p({ node, children }) {
       if (node.children[0].tagName === "img") {
         const { properties, alt } = node.children[0];
 
@@ -32,7 +27,18 @@ function PostContent({ post }) {
         );
       }
 
-      return <p>{paragraph.children}</p>;
+      return <p>{children}</p>;
+    },
+    code({ className, children }) {
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
