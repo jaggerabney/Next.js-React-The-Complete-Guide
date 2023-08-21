80b9c9356ec542ed2c1a637ad34c0f44d15edc82
jaggerabney <jagger.abney@gmail.com>
2023-08-21 10:29:57 -0700

    minified react-syntax-highlighter component

--- course-project/components/posts/PostDetail/PostDetail.js
+++ course-project/components/posts/PostDetail/PostDetail.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 
 import classes from "./PostDetail.module.css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 function PostContent({ post }) {
   const reactMarkdownRenderers = {
