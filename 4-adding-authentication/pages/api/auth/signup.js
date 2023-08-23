dca8fa974777966dc486379da8cc4c3ed4907739
jaggerabney <jagger.abney@gmail.com>
2023-08-23 11:23:16 -0700

    implemented POST route for "/api/auth/signup"

---
+++ 4-adding-authentication/pages/api/auth/signup.js
@@ -0,0 +1,33 @@
+import { hash } from "bcrypt";
+
+import { getDb } from "../../../util/db";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length > 7
+    ) {
+      return res.status(422).json({ message: "Invalid username or password." });
+    }
+
+    try {
+      const db = await getDb();
+
+      const result = await db.collection("users").insertOne({
+        email,
+        password: await hash(password, 12),
+      });
+
+      res
+        .status(201)
+        .json({ message: "Signed up user!", userId: result.insertedId });
+    } catch (err) {
+      return res.status(500).json({ message: "Couldn't sign up!" });
+    }
+  }
+}
