00899adf275d8a06f6f54f14ce13ac4d2ea775bb
jaggerabney <jagger.abney@gmail.com>
2023-08-30 23:52:30 -0700

    options in main navbar depend on login status

--- 4-adding-authentication/pages/_app.js
+++ 4-adding-authentication/pages/_app.js
@@ -1,11 +1,14 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import { SessionProvider } from "next-auth/react";
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
